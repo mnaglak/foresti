1c7fddfbb49a90cb2d0818e5e9335c5f11c44c64
Matt <mnaglak@umich.edu>
2022-02-23 13:45:49 -0500

    point data added; no images

---
+++ forestiPhotos.js
@@ -0,0 +1,845 @@
+var forestiPhotos = {
+   "type": "FeatureCollection",
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -72.94754,-41.4763721 ]
+    },
+    "properties": {
+    "Name":"Puerto Montt-mirador",
+    "Link":"https://postimg.cc/5XZ2HP3P",
+    "NotesNumber":"4"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -72.918325,-41.476633 ]
+    },
+    "properties": {
+    "Name":"Puerto Montt-mirador",
+    "Link":"https://postimg.cc/Q9DNmHv1",
+    "NotesNumber":"5"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -72.9395789,-41.4718097 ]
+    },
+    "properties": {
+    "Name":"Puerto Montt-mirador",
+    "Link":"https://postimg.cc/KRjG0Xpq",
+    "NotesNumber":"10-Jun"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -72.9395789,-41.4718097 ]
+    },
+    "properties": {
+    "Name":"Puerto Montt-mirador",
+    "Link":"https://postimg.cc/jL1s98Gs",
+    "NotesNumber":"10-Jun"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -72.9395789,-41.4718097 ]
+    },
+    "properties": {
+    "Name":"Puerto Montt-mirador",
+    "Link":"https://postimg.cc/mzTRMKFS",
+    "NotesNumber":"10-Jun"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -72.9395789,-41.4718097 ]
+    },
+    "properties": {
+    "Name":"Puerto Montt-mirador",
+    "Link":"https://postimg.cc/d72whJD1",
+    "NotesNumber":"10-Jun"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -72.9395789,-41.4718097 ]
+    },
+    "properties": {
+    "Name":"Puerto Montt-mirador",
+    "Link":"https://postimg.cc/PLsdfCX9",
+    "NotesNumber":"10-Jun"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -72.9707359,-41.3164787 ]
+    },
+    "properties": {
+    "Name":"Puerto Varas",
+    "Link":"https://postimg.cc/pyqVQVC5",
+    "NotesNumber":"11"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -72.9844126,-41.3208267 ]
+    },
+    "properties": {
+    "Name":"Puerto Varas",
+    "Link":"https://postimg.cc/7GX4NX3r",
+    "NotesNumber":"13-Dec"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -72.9844126,-41.3208267 ]
+    },
+    "properties": {
+    "Name":"Puerto Varas",
+    "Link":"https://postimg.cc/FkR4Ccpd",
+    "NotesNumber":"13-Dec"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -73.0242267,-41.138473 ]
+    },
+    "properties": {
+    "Name":"Frutillar-mirador",
+    "Link":"https://postimg.cc/WtbT4LVN",
+    "NotesNumber":"14"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -73.0270573,-41.1391066 ]
+    },
+    "properties": {
+    "Name":"Frutillar",
+    "Link":"https://postimg.cc/4nLZ7L4z",
+    "NotesNumber":"15"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -72.9794365,-41.3635676 ]
+    },
+    "properties": {
+    "Name":"Ensenada",
+    "Link":"https://postimg.cc/CzR0jGd9",
+    "NotesNumber":"16"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -72.4021856,-41.1391122 ]
+    },
+    "properties": {
+    "Name":"Puerto Petrúe",
+    "Link":"https://postimg.cc/VS8cQ24c",
+    "NotesNumber":"17"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -72.2890787,-41.1293295 ]
+    },
+    "properties": {
+    "Name":"Lago Todos Los Santos",
+    "Link":"https://postimg.cc/svbrKMhx",
+    "NotesNumber":"18"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -72.022753,-41.0875302 ]
+    },
+    "properties": {
+    "Name":"Peulla",
+    "Link":"https://postimg.cc/WDwc2Sfq",
+    "NotesNumber":"19"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -72.0205843,-41.0855379 ]
+    },
+    "properties": {
+    "Name":"Hotel de Peulla",
+    "Link":"https://postimg.cc/wyWKNMz6",
+    "NotesNumber":"20"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -72.0167384,-41.0891602 ]
+    },
+    "properties": {
+    "Name":"Camino hacia Peulla",
+    "Link":"https://postimg.cc/BjDGzJTg",
+    "NotesNumber":"21"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.8774065,-41.0594689 ]
+    },
+    "properties": {
+    "Name":"Camino de Rio Frío a Peulla",
+    "Link":"https://postimg.cc/30WsFhr2",
+    "NotesNumber":"24"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.8035164,-41.042252 ]
+    },
+    "properties": {
+    "Name":"Camino de Puerto Blest a Lago Frio",
+    "Link":"https://postimg.cc/Hjx1gw0j",
+    "NotesNumber":"25"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.8534422,-41.0832821 ]
+    },
+    "properties": {
+    "Name":"Frontera",
+    "Link":"https://postimg.cc/4Ypr5BG2",
+    "NotesNumber":"26"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.8029585,-41.0653731 ]
+    },
+    "properties": {
+    "Name":"Lago Frio",
+    "Link":"https://postimg.cc/yJP2qxkF",
+    "NotesNumber":"30"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.801389,-41.066944 ]
+    },
+    "properties": {
+    "Name":"Lago Frio",
+    "Link":"https://postimg.cc/GBr6SGhL",
+    "NotesNumber":"31"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.7884853,-41.0615639 ]
+    },
+    "properties": {
+    "Name":"Camino entre Lago Frio y Lago Nahuel-Huapi",
+    "Link":"https://postimg.cc/v47N9T7p",
+    "NotesNumber":"32"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.8126111,-41.0232948 ]
+    },
+    "properties": {
+    "Name":"Puerto Blest",
+    "Link":"https://postimg.cc/ykZ51vD6",
+    "NotesNumber":"33"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.590772,-40.9391552 ]
+    },
+    "properties": {
+    "Name":"Lago Nahuel Huapi",
+    "Link":"https://postimg.cc/dkNX1fR3",
+    "NotesNumber":"34-35, 37-38, 43"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.590772,-40.9391552 ]
+    },
+    "properties": {
+    "Name":"Lago Nahuel Huapi",
+    "Link":"https://postimg.cc/XBZPygx8",
+    "NotesNumber":"34-35, 37-38, 43"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.590772,-40.9391552 ]
+    },
+    "properties": {
+    "Name":"Lago Nahuel Huapi",
+    "Link":"https://postimg.cc/fkFpQ6Tg",
+    "NotesNumber":"34-35, 37-38, 43"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.590772,-40.9391552 ]
+    },
+    "properties": {
+    "Name":"Lago Nahuel Huapi",
+    "Link":"https://postimg.cc/nXfWCBm4",
+    "NotesNumber":"34-35, 37-38, 43"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.590772,-40.9391552 ]
+    },
+    "properties": {
+    "Name":"Lago Nahuel Huapi",
+    "Link":"https://postimg.cc/FdzW2Zt4",
+    "NotesNumber":"34-35, 37-38, 43"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.5671008,-41.0382658 ]
+    },
+    "properties": {
+    "Name":"Lago Nahuel Huapi-Entrada a Puerto Blest",
+    "Link":"https://postimg.cc/McWrYwdX",
+    "NotesNumber":"39"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.4254839,-41.1086191 ]
+    },
+    "properties": {
+    "Name":"Lago Nahuel Huapi-Puerto Moreno",
+    "Link":"https://postimg.cc/TyCN7s40",
+    "NotesNumber":"40"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.4587769,-41.068117 ]
+    },
+    "properties": {
+    "Name":"Puerto Bueno",
+    "Link":"https://postimg.cc/wyrfHmHs",
+    "NotesNumber":"41"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.4196091,-41.1101418 ]
+    },
+    "properties": {
+    "Name":"Lago Nahuel Huapi Puerto Moreno",
+    "Link":"https://postimg.cc/MnwsWxkt",
+    "NotesNumber":"42"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.3110654,-41.1317013 ]
+    },
+    "properties": {
+    "Name":"Lago Nahuel Huapi, San Carlos",
+    "Link":"https://postimg.cc/9zzKSWV7",
+    "NotesNumber":"36, 44-47, 48-50"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.3110654,-41.1317013 ]
+    },
+    "properties": {
+    "Name":"Lago Nahuel Huapi, San Carlos",
+    "Link":"https://postimg.cc/n9QdLhcS",
+    "NotesNumber":"36, 44-47, 48-50"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.3110654,-41.1317013 ]
+    },
+    "properties": {
+    "Name":"Lago Nahuel Huapi, San Carlos",
+    "Link":"https://postimg.cc/c6V9bwXp",
+    "NotesNumber":"36, 44-47, 48-50"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.3110654,-41.1317013 ]
+    },
+    "properties": {
+    "Name":"Lago Nahuel Huapi, San Carlos",
+    "Link":"https://postimg.cc/yWJpjvXh",
+    "NotesNumber":"36, 44-47, 48-50"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.3110654,-41.1317013 ]
+    },
+    "properties": {
+    "Name":"Lago Nahuel Huapi, San Carlos",
+    "Link":"https://postimg.cc/F73W5z67",
+    "NotesNumber":"36, 44-47, 48-50"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.3110654,-41.1317013 ]
+    },
+    "properties": {
+    "Name":"Lago Nahuel Huapi, San Carlos",
+    "Link":"https://postimg.cc/tszBJfpb",
+    "NotesNumber":"36, 44-47, 48-50"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.3110654,-41.1317013 ]
+    },
+    "properties": {
+    "Name":"Lago Nahuel Huapi, San Carlos",
+    "Link":"https://postimg.cc/v1H3VzZV",
+    "NotesNumber":"36, 44-47, 48-50"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.3110654,-41.1317013 ]
+    },
+    "properties": {
+    "Name":"Lago Nahuel Huapi, San Carlos",
+    "Link":"https://postimg.cc/1gCMcq8Y",
+    "NotesNumber":"36, 44-47, 48-50"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.1434415,-41.0552319 ]
+    },
+    "properties": {
+    "Name":"Estancia San Ramon",
+    "Link":"https://postimg.cc/phmC0L8V",
+    "NotesNumber":"51-59, 78"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.1434415,-41.0552319 ]
+    },
+    "properties": {
+    "Name":"Estancia San Ramon",
+    "Link":"https://postimg.cc/R62R2xQ7",
+    "NotesNumber":"51-59, 78"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.1434415,-41.0552319 ]
+    },
+    "properties": {
+    "Name":"Estancia San Ramon",
+    "Link":"https://postimg.cc/7J7Vktmp",
+    "NotesNumber":"51-59, 78"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.1434415,-41.0552319 ]
+    },
+    "properties": {
+    "Name":"Estancia San Ramon",
+    "Link":"https://postimg.cc/ppbCZxdx",
+    "NotesNumber":"51-59, 78"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.1434415,-41.0552319 ]
+    },
+    "properties": {
+    "Name":"Estancia San Ramon",
+    "Link":"https://postimg.cc/t7VDW7p2",
+    "NotesNumber":"51-59, 78"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.1434415,-41.0552319 ]
+    },
+    "properties": {
+    "Name":"Estancia San Ramon",
+    "Link":"https://postimg.cc/rDq9BJWP",
+    "NotesNumber":"51-59, 78"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.1434415,-41.0552319 ]
+    },
+    "properties": {
+    "Name":"Estancia San Ramon",
+    "Link":"https://postimg.cc/qzsLDYtK",
+    "NotesNumber":"51-59, 78"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.1434415,-41.0552319 ]
+    },
+    "properties": {
+    "Name":"Estancia San Ramon",
+    "Link":"https://postimg.cc/BjNC4KBQ",
+    "NotesNumber":"51-59, 78"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.1434415,-41.0552319 ]
+    },
+    "properties": {
+    "Name":"Estancia San Ramon",
+    "Link":"https://postimg.cc/SXsfsxgL",
+    "NotesNumber":"51-59, 78"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.1434415,-41.0552319 ]
+    },
+    "properties": {
+    "Name":"Estancia San Ramon",
+    "Link":"https://postimg.cc/HVd5nLxJ",
+    "NotesNumber":"51-59, 78"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -60.4054221,-34.6391142 ]
+    },
+    "properties": {
+    "Name":"Chacapuco",
+    "Link":"https://postimg.cc/p96YnMSw",
+    "NotesNumber":"60-61"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -60.4054221,-34.6391142 ]
+    },
+    "properties": {
+    "Name":"Chacapuco",
+    "Link":"https://postimg.cc/5YQ5Bkd6",
+    "NotesNumber":"60-61"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.3470855,-40.3333982 ]
+    },
+    "properties": {
+    "Name":"Campo meliquina",
+    "Link":"https://postimg.cc/PPjbD465",
+    "NotesNumber":"63"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.2755702,-40.6645898 ]
+    },
+    "properties": {
+    "Name":"Rio Truful",
+    "Link":"https://postimg.cc/ppJfJF7Y",
+    "NotesNumber":"62, 64-72"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.2755702,-40.6645898 ]
+    },
+    "properties": {
+    "Name":"Rio Truful",
+    "Link":"https://postimg.cc/wRhhPyBz",
+    "NotesNumber":"62, 64-72"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.2755702,-40.6645898 ]
+    },
+    "properties": {
+    "Name":"Rio Truful",
+    "Link":"https://postimg.cc/8f8RtBNw",
+    "NotesNumber":"62, 64-72"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.2755702,-40.6645898 ]
+    },
+    "properties": {
+    "Name":"Rio Truful",
+    "Link":"https://postimg.cc/6TRLTSfP",
+    "NotesNumber":"62, 64-72"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.2755702,-40.6645898 ]
+    },
+    "properties": {
+    "Name":"Rio Truful",
+    "Link":"https://postimg.cc/RWCcs0xr",
+    "NotesNumber":"62, 64-72"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.2755702,-40.6645898 ]
+    },
+    "properties": {
+    "Name":"Rio Truful",
+    "Link":"https://postimg.cc/SnZcwpmn",
+    "NotesNumber":"62, 64-72"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.2755702,-40.6645898 ]
+    },
+    "properties": {
+    "Name":"Rio Truful",
+    "Link":"https://postimg.cc/Ny5RtP0p",
+    "NotesNumber":"62, 64-72"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.2755702,-40.6645898 ]
+    },
+    "properties": {
+    "Name":"Rio Truful",
+    "Link":"https://postimg.cc/HcM5ccXM",
+    "NotesNumber":"62, 64-72"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.2755702,-40.6645898 ]
+    },
+    "properties": {
+    "Name":"Rio Truful",
+    "Link":"https://postimg.cc/sBq7g9Qy",
+    "NotesNumber":"62, 64-72"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.2755702,-40.6645898 ]
+    },
+    "properties": {
+    "Name":"Rio Truful",
+    "Link":"https://postimg.cc/YLfWR5Xq",
+    "NotesNumber":"62, 64-72"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -70.6478101,-40.4239834 ]
+    },
+    "properties": {
+    "Name":"Cóllun Cura",
+    "Link":"https://postimg.cc/0KqSpk0B",
+    "NotesNumber":"73-75"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -70.6478101,-40.4239834 ]
+    },
+    "properties": {
+    "Name":"Cóllun Cura",
+    "Link":"https://postimg.cc/14pwyF9h",
+    "NotesNumber":"73-75"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -70.6478101,-40.4239834 ]
+    },
+    "properties": {
+    "Name":"Cóllun Cura",
+    "Link":"https://postimg.cc/DSzXQY1n",
+    "NotesNumber":"73-75"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.5555453,-41.9383591 ]
+    },
+    "properties": {
+    "Name":"Valle Nuevo",
+    "Link":"https://postimg.cc/zyPRxSkW",
+    "NotesNumber":"76"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -70.9590426,-40.4073223 ]
+    },
+    "properties": {
+    "Name":"Rio Coleufu",
+    "Link":"https://postimg.cc/F7pSLQdT",
+    "NotesNumber":"77"
+    }
+  }
+]
+}
---
+++ photos.js
@@ -0,0 +1,38 @@
+//This is where you define the map start up options, here defined to center on Paris and to have a particular zoom.
+	var mapOptions = {
+		center: [-38, -71],
+		zoom: 5,
+		maxZoom : 20,
+		minZoom: 4
+	}
+
+//This creates the map variable itself based on the options set above
+	var map = new L.map('map', mapOptions);
+  L.control.pan().addTo(map);
+  
+  var esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+		attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+		}).addTo(map);
+
+
+var photos = L.geoJson(forestiPhotos, {
+  onEachFeature: popUp,
+});
+
+
+function popUp(f,l) {
+	var out = [];
+
+	//adds spaces in between entries
+	if (f.properties) {
+		out.push("Site Name: " + f.properties.Name);
+    out.push("Notes: "+ f.properties.NotesNumber);
+//    out.push('<img src="https://i.postimg.cc/xC0sh031/60.png"> </img>');
+    out.push("<a href='https://postimg.cc/p96YnMSw' target='_blank'><img src='https://i.postimg.cc/p96YnMSw/60.png' border='0' alt='60'/></a>");
+		l.bindPopup(out.join("<br />"));
+	}
+}
+
+			var cluster_places= new L.MarkerClusterGroup({showCoverageOnHover: false});
+				 cluster_places.addLayer(photos);
+				 cluster_places.addTo(map);
