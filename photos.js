29ad89345e2f57c55b2b74c2d414265a1dabf1c3
Matt <mnaglak@umich.edu>
2022-02-25 16:01:04 -0500

    local popups

--- photos.js
+++ photos.js
@@ -9,7 +9,7 @@
 //This creates the map variable itself based on the options set above
 	var map = new L.map('map', mapOptions);
   L.control.pan().addTo(map);
-  
+
   var esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 		attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 		}).addTo(map);
@@ -28,7 +28,7 @@ function popUp(f,l) {
 		out.push("Site Name: " + f.properties.Name);
     out.push("Notes: "+ f.properties.NotesNumber);
 //    out.push('<img src="https://i.postimg.cc/xC0sh031/60.png"> </img>');
-    out.push("<a href='https://postimg.cc/p96YnMSw' target='_blank'><img src='https://i.postimg.cc/p96YnMSw/60.png' border='0' alt='60'/></a>");
+    out.push("<a href='"+ f.properties.PostLink + "' target='_blank'><img src='" + f.properties.LocalLink + "' border='0' alt='60' width='100%'/></a>");
 		l.bindPopup(out.join("<br />"));
 	}
 }
